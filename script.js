16a063f3c72889e7ae1c1ae2e9e643900279dbd5
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-12-03 14:22:52 +0100

    feat: fix functions in js

--- script.js
+++ script.js
@@ -4,27 +4,40 @@
 Attraverso l’apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
-
-**Bonus**
-
-Abbellire con CSS o Bootstrap
-Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
-mostrare le 10 email solo quando solo al termine delle 10 chiamate all’API
 */
 
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+const lista = document.getElementById('lista');
 
-let emails = [];
 
-for(let i = 0 ; i < 10 ; i++){
+function addEmails(){
+  let emails = [];
+  let counter = 0
+  for(let i = 0 ; i < 10 ; i++){
   axios.get(endpoint)
     .then (response=>{
       const email = response.data.response;
       emails.push(email); 
+      counter++;
+      if(counter === 10){
+        addList(emails);
+      }
     })
     .catch (err=>{
       console.log(err);   
     })
+  }
 }
 
-console.log(emails);
+
+function addList(emails){
+  lista.innerHTML = ''
+  emails.forEach(email =>{
+    const element = document.createElement('li');
+    element.textContent = email;
+    lista.append(element)
+  })
+}
+
+
+addEmails();
