68057149c6bce419a9e7f41ee0959b558923a956
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-12-03 13:40:19 +0100

    feat: add axios element in js

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+**Descrizione**
+
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+**Bonus**
+
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+mostrare le 10 email solo quando solo al termine delle 10 chiamate all’API
+*/
+
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+const emails = [];
+
+axios.get(endpoint)
+  .then(response => {
+    const email = response.data.response;
+    for(let i = 0 ; i < 10 ; i++){
+      emails.push(email);
+    }
+    
+  })
+  
+  .catch(e=>{
+    console.log(e);
+  })
+
+  console.log(emails);
+  
